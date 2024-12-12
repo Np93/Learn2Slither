ff3ab83079ff4b33317e787604df6f79b518bbfe
Np93 <nico.hir@hotmail.com>
2024-12-12 18:27:17 +0100

    mise a la norme de game.py

--- src/game.py
+++ src/game.py
@@ -1,18 +1,21 @@
-import time
 import pygame
 from src.board import Board
 from src.display import Display
 
+
 class Game:
-    def __init__(self, board_size=10, display=True, speed=1, victory_condition=10, mode="player"):
+    def __init__(self, board_size=10, display=True,
+                 speed=1, victory_condition=10, mode="player"):
         self.board_size = board_size
         self.display_enabled = display
         self.speed = speed
         self.victory_condition = victory_condition
         self.mode = mode
 
-        # Génération du plateau et récupération du serpent et de la direction initiale
-        self.board = Board(size=board_size, victory_condition=victory_condition)
+        # Génération du plateau et récupération du serpent et
+        # de la direction initiale
+        self.board = Board(size=board_size,
+                           victory_condition=victory_condition)
         self.board.snake, self.direction = self.board.generate_snake()
         self.score = 0
 
@@ -23,7 +26,8 @@ class Game:
             self.margin = 200  # Espace pour le score
             window_width = board_size * self.cell_size + self.margin
             window_height = board_size * self.cell_size
-            self.screen = pygame.display.set_mode((window_width, window_height))
+            self.screen = pygame.display.set_mode((window_width,
+                                                   window_height))
             pygame.display.set_caption("Snake RL")
             self.font = pygame.font.SysFont(None, 36)  # Police pour le score
         else:
@@ -53,7 +57,9 @@ class Game:
                 # Vérification des demi-tours uniquement pour les joueurs
                 if new_direction and self.board.direction:
                     # Calculer si c'est un demi-tour
-                    is_reverse = tuple(map(lambda x, y: x + y, self.board.direction, new_direction)) == (0, 0)
+                    is_reverse = tuple(map(lambda x, y: x + y,
+                                           self.board.direction,
+                                           new_direction)) == (0, 0)
                     if not is_reverse:
                         self.board.update_direction(new_direction)
                     else:
@@ -61,17 +67,23 @@ class Game:
 
     def draw_score(self):
         """Affiche le score à côté du terrain."""
-        score_text = self.font.render(f"Score: {self.score}", True, (255, 255, 255))
+        score_text = self.font.render(f"Score: {self.score}",
+                                      True, (255, 255, 255))
         score_position = (self.board_size * self.cell_size + 20, 20)
         self.screen.blit(score_text, score_position)
 
-    def run(self, agent=None, train=False, current_session=None, total_sessions=None, means_score=None, means_length=None):
+    def run(self, agent=None, train=False, current_session=None,
+            total_sessions=None, means_score=None, means_length=None):
         """
         Boucle principale du jeu.
-        :param agent: Agent Q-learning si en mode `train` ou `model`.
-        :param train: Indique si le jeu est en mode `train` (True) ou `model` (False).
-        :param current_session: Numéro de la session actuelle (utilisé en mode train).
-        :param total_sessions: Nombre total de sessions (utilisé en mode train).
+        :param agent: Agent Q-learning si en mode
+        `train` ou `model`.
+        :param train: Indique si le jeu est en mode
+        `train` (True) ou `model` (False).
+        :param current_session: Numéro de la session actuelle
+        (utilisé en mode train).
+        :param total_sessions: Nombre total de sessions
+        (utilisé en mode train).
         """
         clock = pygame.time.Clock()
         start_time = pygame.time.get_ticks()
@@ -135,7 +147,7 @@ class Game:
                     score = self.score
                     return length, score
                 break
-            
+
             # self.print_action(action)
             # self.board.get_vision()
 
@@ -182,23 +194,25 @@ class Game:
                 reason=reason
             )
         else:
-            # Afficher les informations dans la console si l'affichage est désactivé
+            # Afficher les informations si l'affichage est désactivé
             print(f"{title}!")
             print(f"Reason: {reason}")
             print(f"Score: {self.score}")
             print(f"Time elapsed: {elapsed_time:.1f}s")
             print(f"Snake length: {len(self.board.snake.get_body())}")
-    
+
     def reset(self):
         """
         Réinitialise l'état du jeu pour une nouvelle session d'entraînement.
         """
         # Réinitialiser le plateau en utilisant Board
-        self.board = Board(size=self.board_size, victory_condition=self.victory_condition)
-        
+        self.board = Board(size=self.board_size,
+                           victory_condition=self.victory_condition)
+
         # Récupérer le serpent et la direction initiale depuis le Board
-        self.board.snake, self.direction = self.board.snake, self.board.direction
-        
+        self.board.snake = self.board.snake
+        self.direction = self.board.direction
+
         # Réinitialiser le score
         self.score = 0
 
